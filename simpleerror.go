479f7ffe985a28f917ecf159e5de2d8c886290f6
Arnau Canyadell <arnau.canyadell.miquel@hp.com>
2021-04-21 13:59:32 +0200

    documentation improvements

--- simpleerror.go
+++ simpleerror.go
@@ -5,23 +5,23 @@ type simpleError string
 // SimpleError returns a simple error useful to define constant simple errors that will wrap other errors.
 //
 // ```go
-// const MyError = wraperrors.simpleError("my error message")
+// const MyError = wraperrors.SimpleError("my error message")
 // ```
 func SimpleError(str string) error {
 	return simpleError(str)
 }
 
+// Error returns the simple error's string and allows simple errors to implement the error interface
 func (err simpleError) Error() string {
 	return string(err)
 }
 
-// Is compares equality of err and target
+// Is compares the equality of the simple error and a target
 func (err simpleError) Is(target error) bool {
 	return err == target
 }
 
-// Wrap wraps the wrappedErr into err, creating an error that has err as its wrapper error and wrappedErr as its wrapped
-// error
+// Wrap creates a wrap error that has this simple error as wrapper error and wrapped as the wrapped error
 func (err simpleError) Wrap(wrapped error) error {
 	return New(err, wrapped)
 }
--- wraperror.go
+++ wraperror.go
@@ -15,17 +15,23 @@ func New(wrapper, wrapped error) error {
 	}
 }
 
+// Error returns a string describing the wrap error and allows warp errors to implement the error interface
 func (err *wrapError) Error() string {
 	return err.wrapper.Error() + ", " + err.wrapped.Error()
 }
 
-// Is compares the equality of err and target, or the equality of the wrapper error in err with target, or the equality
-// of the wrapped error in err and target
+// Is compares the equality of a warp error to a target error. This method allows wrap errors to implement ths errors Is
+// interface, and allows wrap errors to be compared using `errors.Is(myWrapError, anyOtherError)`
+//
+// A wrap error is equal to a target error if:
+// (a) the wrap error is exactly equal (== operator) to the target error,
+// (b) the wrapper error is equal to the target error using the errors.Is function, or
+// (c) the wrapped error is equal to the target error using the errors.Is function
 func (err *wrapError) Is(target error) bool {
 	return err == target || errors.Is(err.wrapper, target) || errors.Is(err.wrapped, target)
 }
 
-// Unwrap unwraps the wrapped error
+// Unwrap returns the wrapped error
 func (err *wrapError) Unwrap() error {
 	return err.wrapped
 }
